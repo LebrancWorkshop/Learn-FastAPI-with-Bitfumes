069ae20779adee5601c0b80e3246b3c9de119197
lebrancconvas <lebrancconvas@gmail.com>
2023-05-02 20:05:05 +0700

    :tada: Init.

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+  return {"message": "Hello, World!"}
+
+@app.get("/about")
+def about():
+  return {"message": "About this API"}
